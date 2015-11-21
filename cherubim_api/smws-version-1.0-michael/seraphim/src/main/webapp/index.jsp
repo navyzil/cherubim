bf6f192bac4e5f5a3283affd1ecd2a680c91e603
Denzil Daulo <navyzil@yahoo.com>
2015-11-21 12:08:23 +0800

    Cherubim Api - added seraphim engine
    Signed-off-by: Denzil Daulo <navyzil@yahoo.com>

---
+++ cherubim_api/smws-version-1.0-michael/seraphim/src/main/webapp/index.jsp
@@ -0,0 +1,86 @@
+<!-- 
+Copyright 2014 Denzil Gideon M. Daulo
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+@author: Denzil Gideon M. Daulo
+@since: ver 1.0
+ -->
+
+<html>
+<head>
+<script type="text/javascript" src="js/jquery/jquery-2.1.3.min.js"></script>
+<script type="text/javascript" src="js/jquery-elastic-1.6.10/jquery.elastic.source.js"></script>
+<script type="text/javascript" src="js/jquery.autogrowtextarea.js"></script>
+<script type="text/javascript" src="js/seraphim-ui-behavior.js"></script>
+<link rel="stylesheet" type="text/css" href="css/seraphim.css"/>
+</head>
+<body>
+<h2>Welcome to Seraphim Engine (ver1.0-Michael)</h2>
+<div id="basicDetails">
+	<label>Enter IP Address (default:localhost):</label><input type="text" name="ipAddress" id="ipAddress" value="localhost">
+	<label>Enter Port (default:8080):</label><input type="text" name="port" id="port" value="8080">
+	<br>
+	<label>Enter Api:</label><input type="text" name="apiAddress" id="apiAddress">
+	<br>
+	<label>Select HTTP Methods:</label>
+	<select id="httpType" name="httpType">
+		<option value="GET">GET</option>
+		<option value="POST">POST</option>
+		<option value="PUT">PUT</option>
+		<option value="DELETE">DELETE</option>
+	</select>
+</div>
+<br/>
+
+<div>
+	<table>
+		<tr>
+			<td style="width: 750px;">
+				<label>Parameters (optional):</label>
+				<div id="parameterLabel" name="parameterLabel">
+<!-- 					<label>Parameter Name:</label>&nbsp;&nbsp;<label>Parameter Value:</label>
+ -->				<table>
+					<tr>
+						<td style="width: 175px;">
+							<label>Parameter Name:</label>
+						</td>
+						<td style="width: 175px;">
+							<label>Parameter Value:</label>
+						</td>
+					</tr>
+				</table>
+				</div>
+				<div id="parameterNamesAndValues" name="parameterNamesAndValues">
+				</div>				
+				<div>
+					<table id="parameterTable" name="parameterTable">
+					</table>
+					<button id="addParameter" onclick="addParameter();"><img id="plusImg" name="plusImg" alt="" src="assets/images/plus.png"></button>
+					<button id="removeParameter" onclick="removeParameter();"><img id="minusImg" name="minusImg" alt="" src="assets/images/minus.png"></button>
+				</div>
+				<br/>
+				<div id="apiExecution">
+					<button id="executeApi" name="executeApi" onclick="runApi();"><img id="runImg" name="runImg" alt="" src="assets/images/runApi.png"></button>
+				</div>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<div id="transactionOutput" name="transactionOutput">	
+				</div>
+ 			</td>
+		</tr>
+	</table>
+</div>
+</body>
+</html>
---
+++ cherubim_api/smws-version-1.0-michael/seraphim/src/main/webapp/js/seraphim-ui-behavior.js
@@ -0,0 +1,182 @@
+/**
+ * Copyright 2014 Denzil Gideon M. Daulo
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+ *@author: Denzil Gideon M. Daulo
+ *@since: ver 1.0
+ */
+
+var parameterCount = 0;
+$(function(){
+	
+});
+
+function addParameter(){
+		var htmlText=
+		'<tr id="parameterRow'+parameterCount+'" name="parameterRow'+parameterCount+'">'+
+			'<td style="width: 175px;">'+
+				'<input type="text" name="parameterName'+parameterCount+'" id="parameterName'+parameterCount+'">' +
+			'</td>'+
+			'<td style="width: 175px;">'+
+				'<input type="text" name="parameterValue'+parameterCount+'" id="parameterValue'+parameterCount+'">' +
+			'</td>'+
+		'</tr>';
+//		'<br id="parameterBreakCount'+parameterCount+'" name="parameterBreakCount'+parameterCount+'"/>';
+//		$("#parameterNamesAndValues").append(htmlText);
+		$("#parameterTable").append(htmlText);
+		parameterCount++;
+}
+
+function removeParameter(){
+	if(parameterCount>=0){
+		parameterCount--;		
+
+		var parameterName = "#parameterName"+parameterCount;
+		var parameterValue = "#parameterValue"+parameterCount;
+//		var parameterBreakCount = "#parameterBreakCount"+parameterCount;
+		var parameterRow = "#parameterRow"+parameterCount;
+
+		$(parameterName).remove();
+		$(parameterValue).remove();
+//		$(parameterBreakCount).remove();
+		$(parameterRow).remove();
+
+	}
+
+}
+
+function runApi(){
+	var ipAddress = $("#ipAddress").val();
+	var port = $("#port").val();
+	var apiAddress = $("#apiAddress").val();
+	var urlValue = "http://"+ipAddress+":"+port+"/seraphim/api/"+apiAddress;	
+	var httpType = $("#httpType").val();
+	var parameters=parseParameters();
+	
+	executeAjax(httpType, urlValue, parameters);
+}
+
+function parseParameters(){
+	var data="";//var data={};
+	for(i=0; i < parameterCount; i++){
+		if(i === 0){
+			data = "?";
+		}
+		var parameterName = $("#parameterName"+i).val();
+		var parameterValue = $("#parameterValue"+i).val();
+		
+//		alert("parameterName:"+parameterName+" parameterValue:"+parameterValue);
+		//data[parameterName]=parameterValue;
+		data+=parameterName+"="+parameterValue;
+		if(i < (parameterCount-1)){
+			data+="&";
+		}
+	}
+	return data;
+}
+
+function executeAjax(httpType, urlValue, parameters){
+	
+	jQuery.ajax({
+        type: httpType,
+        url: urlValue+parameters,
+        contentType: "application/json; charset=utf-8",//"application/x-www-form-urlencoded; charset=utf-8",//"application/json; charset=utf-8",
+        //data: JSON.stringify(data),
+        dataType: "json",
+        success: function (data, status) {
+        	//success output
+        	$("#result").remove();
+        	var parsed_data = JSON.stringify(data); 
+        	parsed_data=parsed_data.replace(/"/g, "");
+    		var htmlText=
+//    		'<div id="result">'+
+//    			'<label>Result:</label><br/>'+
+//    			'<label>STATUS:</label>&nbsp;&nbsp;&nbsp;'+status.toUpperCase()+'</label>'+
+//    			'<br/>'+
+//    			'<label>RESPONSE CODE:</label>'+
+//			 		200+
+//			 		'<br/>'+
+//			 		'<label>DATA OUTPUT:</label>'+
+//			 		'<br/>'+
+//			 		parsed_data+
+//			 "</div>";
+
+        		'<div id="result">'+
+        		'<label>Result:</label><br/>'+
+        		'<textarea id="textResult" readonly style="height: 250; width: 750;">'+
+    			
+    			'STATUS:&nbsp;&nbsp;&nbsp;'+status.toUpperCase()+
+    		
+    			'\nRESPONSE CODE: 200'+
+    			'\nDATA OUTPUT:'+
+			 		parsed_data+
+			 	"</textarea>"+
+			 	'<button id="closeResult" name="closeResult" onclick="closeWindow();"><img id="closeImg" name="closeImg" alt="" src="assets/images/close.png"></button>'+
+		 		"</div>";
+//    			'<jsp:include page="result.jsp"></jsp:include>';
+//    			"<iframe id='result' name='result' width='750' height='250' frameborder='0' src='results.jsp?status="+status.toUpperCase()+
+//    			"&responseCode=200"+
+//    			"&data="+parsed_data+
+//    			"'></iframe>";
+//    		$("#textResult").elastic();
+    		$("#transactionOutput").append(htmlText);
+
+        },
+
+        error: function (data, status) {
+            // error handler
+        	$("#result").remove();
+        	//var parsed_data = "<!-- "+JSON.stringify(data);    
+        	var parsed_data = JSON.stringify(data);     
+        	parsed_data=parsed_data.replace(/"/g, "");
+    		var htmlText=
+//        		'<div id="result">'+
+//        			'<label>Result:</label><br/>'+
+//        			'<label>STATUS:</label>&nbsp;&nbsp;&nbsp;'+status.toUpperCase()+'</status>'+
+//        			'<br/>'+
+//        			'<label>RESPONSE CODE:</label>'+
+//    			 		data.status+
+//    			 		'<br/>'+
+//    			 		'<label>DATA OUTPUT:</label>'+
+//    			 		'<br/>'+
+//    			 		parsed_data+
+//    			 "</div>";    	
+        		'<div id="result" width="750" height="250">'+
+        		'<label>Result:</label><br/>'+
+        		'<textarea id="textResult" readonly style="height: 250; width: 750;">'+
+    			
+    			'STATUS:&nbsp;&nbsp;&nbsp;'+status.toUpperCase()+
+    		
+    			'\nRESPONSE CODE: '+data.status+
+    			'\nDATA OUTPUT:'+
+			 		parsed_data+
+			 	"</textarea>"+
+			 	'<button id="closeResult" name="closeResult" onclick="closeWindow();"><img id="closeImg" name="closeImg" alt="" src="assets/images/close.png"></button>'+
+		 		"</div>";
+
+//    		$("#textResult").elastic();
+//    			'<iframe id="result" name="result" width="750" height="250" frameborder="0" src="results.jsp?status='+status.toUpperCase()+
+//    			'&responseCode='+data.status+
+//    			'&data='+parsed_data+
+//    			'"></iframe>';
+
+    		$("#transactionOutput").append(htmlText);
+        }
+	 });
+}
+
+function closeWindow(){
+	$("#result").remove();	
+}
+
